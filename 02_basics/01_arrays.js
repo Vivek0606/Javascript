75dab0876ca5d155c71194274b3ec5a34fcc3605
Vivek Poojary <79552829+Vivek0606@users.noreply.github.com>
2023-12-10 21:15:33 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -2,28 +2,28 @@ const myArr = [0,1,2,3,4,5]
 const myHeroes = ['Ironaman', "Dean", "Thor"]
 
 const myArr2 = new Array(1,2,3,4)
-//console.log(myArr[0]);
-//console.log(myHeroes[1]);
-//console.log(myArr2[3]);
+console.log(myArr[0]);
+console.log(myHeroes[1]);
+console.log(myArr2[3]);
 
 
 // Array Methods
 
 
-//myArr.push(6)
-//myArr.push(9)
-//myArr.pop()
+myArr.push(6)
+myArr.push(9)
+myArr.pop()
 
-//myArr.unshift(7)
-//myArr.shift()
+myArr.unshift(7)
+myArr.shift()
 
-//console.log(myArr.includes(9));
+console.log(myArr.includes(9));
 
-//const newArr = myArr.join() // converts array to string
+const newArr = myArr.join() // converts array to string
 
 
-//console.log(myArr);
-//console.log(newArr);
+console.log(myArr);
+console.log(newArr);
 
 
 
