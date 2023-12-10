5ee6d114f7420e85598420186d30b65c7a4d4606
Vivek Poojary <79552829+Vivek0606@users.noreply.github.com>
2023-12-10 20:32:09 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,7 @@
+const myArr = [0,1,2,3,4,5]
+const myHeroes = ['Ironaman', "Dean", "Thor"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[0]);
+console.log(myHeroes[1]);
+console.log(myArr2[3]);
