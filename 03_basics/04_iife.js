215216a15a175de08520931049b6cf32c6d26a5f
Vivek Poojary <79552829+Vivek0606@users.noreply.github.com>
2023-12-12 07:24:36 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,8 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    console.log('Db connected');
+} )();
+
+// Many times variables from global scope cause pollution to prevent that we use IIFE 
