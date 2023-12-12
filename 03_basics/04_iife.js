47aac5e1e0d054cbe9743f1990a64a00c895cfd2
Vivek Poojary <79552829+Vivek0606@users.noreply.github.com>
2023-12-12 07:30:44 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,8 +1,16 @@
 // Immediately Invoked Function Expressions (IIFE)
 
-
+//Named IIFE
 (function chai(){
     console.log('Db connected');
 } )();
+// Use semicolon to execute next IIFE code.
+
+
+
+
+( (name) => {
+    console.log(`DB connected TWO ${name}`);
+} )('VIVEK')
 
 // Many times variables from global scope cause pollution to prevent that we use IIFE 
